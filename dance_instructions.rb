5b77d7a811a7b2eb196e3c179c0ad50959e044ce
ava azama <avaazama@gmail.com>
2018-06-19 13:41:31 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,9 @@
-
+# Gangnam Style
 # Code your methods below
+def proper_stance
+puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def base_footwork 
+puts "Lift right foot"
+puts "Return right foot to the ground
+puts "Finishing with a small skip-step backward"
